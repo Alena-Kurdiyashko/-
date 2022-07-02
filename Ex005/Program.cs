7def67320c76cd9b7a2261d077be69256b493cb0
Alena <alesikxxx@mail.ru>
2022-07-02 23:47:12 +0300

    change

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numderB = 5;
+﻿int numberA = new Random().Next(1, 10);   // 1 2 3 ... 9
+Console.WriteLine(numberA);
+int numderB = new Random().Next(1, 10);
+Console.WriteLine(numderB);
 int result = numberA + numderB;
 Console.WriteLine(result);
 
