45bda1201b9f56002de247c6f008e2c865ff32ef
Alena <alesikxxx@mail.ru>
2022-07-03 00:05:14 +0300

    Marley

---
+++ Ex007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "marley")
+{
+    Console.WriteLine("Ура, это же my love");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
